5def0ceae97a9f293ac1a579eefe6f89580a5898
Juan Velasco <juanayevela@yahoo.com>
2023-03-16 23:43:09 -0400

    Fix temperature conversion from Fahrenheit to Celsius

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 document.getElementById('fetchWeather').addEventListener('click', async () => {
     const city = document.getElementById('city').value;
     const url = `/api/weather?city=${city}`;
-
+    
     try {
-      const response = await fetch(url);
-      const data = await response.json();
-      const celsius = data.main.temp.toFixed(1);
-      const fahrenheit = (celsius * 9 / 5 + 32).toFixed(1);
-      const weatherInfo = `
-        <h2>${data.name}, ${data.sys.country}</h2>
-        <p>${data.weather[0].main}</p>
-        <p>Temperature: ${fahrenheit}°F (${celsius}°C)</p>
-        <p>Humidity: ${data.main.humidity}%</p>
-      `;
-      document.getElementById('weatherInfo').innerHTML = weatherInfo;
+        const response = await fetch(url);
+        const data = await response.json();
+        const fahrenheit = data.main.temp.toFixed(1);
+        const celsius = ((fahrenheit - 32) * (5 / 9)).toFixed(1);
+        const weatherInfo = `
+            <h2>${data.name}, ${data.sys.country}</h2>
+            <p>${data.weather[0].main}</p>
+            <p>Temperature: ${fahrenheit}°F (${celsius}°C)</p>
+            <p>Humidity: ${data.main.humidity}%</p>
+        `;
+    document.getElementById('weatherInfo').innerHTML = weatherInfo;
     } catch (error) {
-    console.error('Error fetching weather data:', error);
+        console.error('Error fetching weather data:', error);
     }
 });
