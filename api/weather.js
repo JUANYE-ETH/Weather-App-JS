f0435c5798d2705a8c1ee78348f26ee1b982e4f0
Juan Velasco <juanayevela@yahoo.com>
2023-03-16 23:28:17 -0400

    added a try-catch block for error handling

--- api/weather.js
+++ api/weather.js
@@ -1,20 +1,14 @@
-const fetch = require('node-fetch');
+const axios = require('axios');
 
 module.exports = async (req, res) => {
     const city = req.query.city;
     const apiKey = process.env.OPEN_WEATHER_API_KEY;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
 
     try {
-        const response = await fetch(url);
-        const data = await response.json();
-
-    if (response.ok) {
-        res.status(200).json(data);
-    } else {
-        res.status(response.status).json(data);
+        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`);
+        res.status(200).json(response.data);
+    } catch (error) {
+        console.error('Error fetching weather data:', error.message);
+        res.status(500).json({ message: error.message });
     }
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
 };
