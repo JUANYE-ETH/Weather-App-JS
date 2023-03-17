4d45b11a860ccb22890ed252eb1889e9f56e82fd
Juan Velasco <juanayevela@yahoo.com>
2023-03-16 23:36:17 -0400

    added package.json to install axios

--- api/weather.js
+++ api/weather.js
@@ -9,6 +9,6 @@ module.exports = async (req, res) => {
         res.status(200).json(response.data);
     } catch (error) {
         console.error('Error fetching weather data:', error.message);
-        res.status(500).json({ message: error.message });
+        res.status(500).json({ error: error.message });
     }
 };
